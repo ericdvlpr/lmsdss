50f6fcc1abba30161cb82b17f9ab639cefad3cda
Eric Paul Jaucian <EricPaul.Jaucian@greymouse.local>
2017-11-08 18:42:05 +1000

    Select Book data

--- core/database.php
+++ core/database.php
@@ -26,22 +26,31 @@
            $output .= '  
            <table class="table table-bordered table-striped">  
                 <tr>  
-                     <th width="10%">Image</th>  
-                     <th width="35%">First Name</th>  
-                     <th width="35%">Last Name</th>  
-                     <th width="10%">Update</th>  
-                     <th width="10%">Delete</th>  
+                     <th width="10%">Book Catalog No</th>  
+                     <th width="10%">Book Title</th>  
+                     <th width="35%">Book Author</th>  
+                     <th width="35%">Book Published</th>  
+                     <th width="35%">Publisher Name</th>  
+                     <th width="35%">Date Added</th>  
+                     <th width="35%">Book Copies</th>  
+                     <th width="35%">Status</th>  
+                      
+                     <th width="10%">Command</th>  
                 </tr>  
            ';  
            while($row = mysqli_fetch_object($result))  
            {  
                 $output .= '  
-                <tr>       
-                     <td><img src="upload/'.$row->image.'" class="img-thumbnail" width="50" height="35" /></td>  
-                     <td>'.$row->first_name.'</td>  
-                     <td>'.$row->last_name.'</td>  
-                     <td><button type="button" name="update" id="'.$row->id.'" class="btn btn-success btn-xs update">Update</button></td>  
-                     <td><button type="button" name="delete" id="'.$row->id.'" class="btn btn-danger btn-xs delete">Delete</button></td>  
+                <tr>        
+                     <td>'.$row->book_id.'</td>  
+                     <td>'.$row->book_title.'</td>  
+                     <td>'.$row->author.'</td>  
+                     <td>'.$row->book_pub.'</td>  
+                     <td>'.$row->publisher_name.'</td>  
+                     <td>'.$row->date_added.'</td>  
+                     <td>'.$row->book_copies.'</td>  
+                     <td>'.$row->status.'</td>  
+                     <td><button type="button" name="update" id="'.$row->book_id.'" class="btn btn-success btn-xs update">Update</button><button type="button" name="delete" id="'.$row->book_id.'" class="btn btn-danger btn-xs delete">Delete</button></td>  
                 </tr>  
                 ';  
            }  
